39502d521ea9a31911c82806edf484e3a0bf48df
Juan-cruz22 <ghigliani02@gmail.com>
2024-10-17 13:02:44 -0300

    Menú.js

---
+++ src/Menú.js
@@ -0,0 +1,42 @@
+export default [
+    {
+       id: 1,
+       name: 'Marinara',
+       ingredientes: ['Salsa de tomate','Oregano', 'Ajo'],
+       price: '5.000',
+       description: 'Abundante salsa de tomate, oregano y ajo.',
+       image: '',
+    },
+    {
+        id: 2,
+        name: 'Margherita',
+        ingredientes: ['Salsa de tomate','Mozzarella', 'Albahaca'],
+        price: '6.000',
+        description: 'Mozzarella, Salsa de tomate casera y albahaca fresca',
+        image: '',
+    },
+    {
+        id: 3,
+        name: 'Muzza',
+        ingredientes: ['Salsa de tomate','Mozzarella', 'Oregano'],
+        price: '6.000',
+        description: 'Mozzarella, Salsa de tomate y oregano',
+        image: '',
+    },
+    {
+        id: 4,
+        name: 'Diávola',
+        ingredientes: ['Salsa de tomate picante dulce','Mozzarella', 'Pepperoni', 'Queso parmesano'],
+        price: '6.800',
+        description: 'Base de salsa de tomate, Mozzarella, Pepperoni, Queso parmesano y el pesto Diavola dulce y picante.',
+        image: '',
+    },
+    {
+        id: 5,
+        name: 'Caprese',
+        ingredientes: ['Salsa de tomate','Mozzarella', 'Albahaca', 'Tomates Cherry'],
+        price: '6.500',
+        description: 'Mozzarella, Salsa de tomate, Tomates cherry y albahaca.',
+        image: '',
+    },
+ ];
