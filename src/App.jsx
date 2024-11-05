f6dfb3c48047e694f358e2a560546e4957b62d68
Juan-cruz22 <ghigliani02@gmail.com>
2024-11-05 15:26:26 -0300

    Creacion de archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route path='/menu'/>
+        <Route path='/detalle'/>
+        <Route path='/carrito'/>
+      </Routes>
     </>
   )
 }
---
+++ src/Front/ComponenteMenu/ComponenteMenu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ComponenteMenu (props){
+
+    const {id, name, ingredientes, price, description} = props;
+
+
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default ComponenteMenu;
